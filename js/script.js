87ec228924680d86c1374cdf08a6601f26fb6faf
DanielExe78 <100711304+DanielExe78@users.noreply.github.com>
2022-08-24 17:57:55 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,52 @@
+"use strict";
+
+// MOBILE NAVIGATION
+const mobileNav = document.querySelector(".header");
+const btnNav = document.querySelector(".btn-mobile-nav");
+const section1 = document.querySelector(".section-featured");
+const footer = document.querySelector(".footer");
+
+btnNav.addEventListener("click", function () {
+  mobileNav.classList.toggle("nav-open");
+});
+
+// STICKY NAV
+const footerHeight = footer.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) mobileNav.classList.remove("sticky");
+  else mobileNav.classList.add("sticky");
+};
+
+const obsOptions = {
+  root: null,
+  threshold: [0.5, 1],
+  rootMargin: `${footerHeight}px`,
+};
+
+const observer = new IntersectionObserver(stickyNav, obsOptions);
+observer.observe(section1);
+
+// REVEAL SECTION
+const allSections = document.querySelectorAll(".section");
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach((section) => {
+  sectionObserver.observe(section);
+
+  section.classList.add("section--hidden");
+});
